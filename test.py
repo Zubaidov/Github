2532b3c271cd1569e64a2b90dc27bc993ada6cf0
Zubaydov <shohruz.zubaidov@gmail.com>
2022-06-28 20:08:26 +0400

    all changes are done

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+git config --global user.eamil [your email which connected to the git]
+
 git status # showing the changes inside your files
 
 git add [name of the file].fornat - # you adding  a new file
 
 git commit -m "Second commit" [name of the file].fornat #commenting after addind file to the git
 
-git push -u origin main #pushing your job in your github
+git push -u origin main #pushing your git data to your github
 
