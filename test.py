37e731dac1ba20c1e7b4d68a8e45fbabc5f82c29
Zubaydov <shohruz.zubaidov@gmail.com>
2022-06-28 19:35:20 +0400

    new line of code are added

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ git status # showing the changes inside your files
 
 git add [name of the file].fornat - # you adding  a new file
 
-git commit -m "Second commit" [name of the file].fornat
+git commit -m "Second commit" [name of the file].fornat #commenting after addind file to the git
+
+git push -u origin main #pushing your job in your github
+
